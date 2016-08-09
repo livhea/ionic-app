09d4d67720b4f53953f55b95521e09f530726013
swapnil agarwal <swapnil.agarwal@wingify.com>
2016-08-09 13:23:31 +0530

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,115 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+
+        var helpshiftAppId = 'livhea_platform_20160801163533010-9c4746eacfa9218';
+        if (ionic.Platform.isIOS()) {
+            helpshiftAppId = 'livhea_platform_20160727123716116-b102dadbed53db1';
+        }
+
+        // Initializing HelpShift sdk
+        var helpshiftConfig = {
+            "disableEntryExitAnimations": "YES", 
+            "requireEmail": "NO",
+            "hideNameAndEmail": "YES",
+            "conversationPrefillText": "Hi Swapnil here, How can I of assistance ?"
+        };
+
+        window.HelpshiftPlugin.install("e86b51fb7b165e636ab022fcbb9e3703", 
+                                        "livhea.helpshift.com", 
+                                        helpshiftAppId, 
+                                        helpshiftConfig);
+    });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+    // setup a state for launch screen
+    .state('launch', {
+        url: '/launch',
+        // abstract: true,
+        templateUrl: 'templates/launch.html',
+        controller: 'LaunchController'
+    })
+
+    // setup an abstract state for the tabs directive
+    .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+    })
+
+    // Each tab has its own nav history stack:
+    .state('tab.dash', {
+        url: '/dash',
+        views: {
+            'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+            }
+        }
+    })
+
+    .state('tab.chats', {
+        url: '/chats',
+        views: {
+            'tab-chats': {
+                templateUrl: 'templates/tab-chats.html',
+                controller: 'ChatsCtrl'
+            }
+        }
+    })
+
+    .state('tab.more', {
+        url: '/more',
+        views: {
+            'tab-more': {
+                templateUrl: 'templates/tab-more.html',
+                controller: 'MoreCtrl'
+            }
+        }
+    })
+
+    .state('tab.more-detail', {
+        url: '/more/:blogId',
+        views: {
+            'tab-more': {
+                templateUrl: 'templates/blog-detail.html',
+                controller: 'BlogDetailCtrl'
+            }
+        }
+    })
+
+    console.log('window.localStorage.starter_facebook_user -->', window.localStorage.starter_facebook_user);
+    // if none of the above states are matched, use this as the fallback
+    if(window.localStorage.starter_facebook_user) {
+        $urlRouterProvider.otherwise('/tab/dash');
+    } else {
+        $urlRouterProvider.otherwise('/launch');
+    }
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,180 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, Blogs) {
+	$scope.blogs = Blogs.all();
+})
+
+.controller('ChatsCtrl', function($scope) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  // $scope.$on('$ionicView.enter', function(e) {
+  // });
+
+  // $scope.$on('$viewContentLoaded', function() {
+  //     //call it here
+  //     window.HelpshiftPlugin.showConversation();
+  // });
+
+	$scope.showConversation = function() {
+		console.log('chats controller showConversation()');
+		window.HelpshiftPlugin.showConversation();
+	};
+})
+
+.controller('MoreCtrl', function($scope, Blogs) {
+	$scope.blogs = Blogs.all();
+})
+
+.controller('BlogDetailCtrl', function($scope, $stateParams, Blogs) {
+	var blog = Blogs.get($stateParams.blogId);
+  $scope.blog = blog;
+
+  $scope.thisCanBeusedInsideNgBindHtml = blog.content;
+})
+
+// .controller('LaunchController', function($scope, $state) {
+
+//   $scope.loginWithFacebook = function(){
+//     console.log('loginWithFacebook');
+//     $state.go('tab.dash');
+//   };
+
+//   $scope.skipLogin = function(){
+//     console.log('skipLogin');
+//     $state.go('tab.dash');
+//   };
+
+
+//   $scope.settings = {
+//     enableFriends: true
+//   };
+// })
+
+// https://ionicthemes.com/tutorials/about/native-facebook-login-with-ionic-framework
+.controller('LaunchController', function($scope, $state, $q, UserService, $ionicLoading, $ionicPopup) {
+  // This is the success callback from the login method
+	var fbLoginSuccess = function(response) {
+	  	if (!response.authResponse){
+	  		fbLoginError("Cannot find the authResponse");
+	  		return;
+	  	}
+
+	  	var authResponse = response.authResponse;
+
+	  	getFacebookProfileInfo(authResponse)
+	  	.then(function(profileInfo) {
+	  		console.log('profileInfo:', profileInfo);
+
+	  		setHelpshiftInformation(profileInfo.name, profileInfo.email);
+		  	// For the purpose of this example I will store user data on local storage
+		 	UserService.setUser({
+			  	authResponse: authResponse,
+			  	userID: profileInfo.id,
+			  	name: profileInfo.name,
+			  	email: profileInfo.email,
+			  	picture : "https://graph.facebook.com/" + authResponse.userID + "/picture?type=large"
+		  });
+		  $ionicLoading.hide();
+		  $state.go('tab.dash');
+		}, function(fail){
+		  	// Fail get profile info
+			console.log('profile info fail', fail);
+		});
+  	};
+
+  	// This is the fail callback from the login method
+	var fbLoginError = function(error){
+	  	console.log('fbLoginError', error);
+	  	$ionicLoading.hide();
+
+		var alertPopup = $ionicPopup.alert({
+			title: 'Facebook Login',
+		 	template: 'Sorry, an error occurred. Please try again.'
+		});
+
+		alertPopup.then(function(res) {
+		 	console.log('Thank you for not eating my delicious ice cream cone');
+		});
+
+  	};
+
+  	// This method is to get the user profile info from the facebook api
+	var getFacebookProfileInfo = function (authResponse) {
+	  	var info = $q.defer();
+
+	  	facebookConnectPlugin.api('/me?fields=email,name&access_token=' + authResponse.accessToken, null,
+	  		function (response) {
+	  			console.log(response);
+	  			info.resolve(response);
+	  		},
+	  		function (response) {
+	  			console.log(response);
+	  			info.reject(response);
+	  		}
+	  	);
+	  	return info.promise;
+  	};
+
+  	var setHelpshiftInformation = function(name, email) {
+		// set user info for the helpshift
+		window.HelpshiftPlugin.setNameAndEmail(name, email);
+  	};
+
+  	//This method is executed when the user press the "Login with facebook" button
+ 	$scope.facebookSignIn = function() {
+  		facebookConnectPlugin.getLoginStatus(function(success){
+	  		if(success.status === 'connected') {
+				// The user is logged in and has authenticated your app, and response.authResponse supplies
+				// the user's ID, a valid access token, a signed request, and the time the access token
+				// and signed request each expire
+				console.log('getLoginStatus', success.status);
+
+				// Check if we have our user saved
+				var user = UserService.getUser('facebook');
+				console.log('user:', JSON.stringify(user));
+
+				if(!user.userID){
+				  // user is not saved, get user profile information and save it
+					getFacebookProfileInfo(success.authResponse)
+					.then(function(profileInfo) {
+
+						// For the purpose of this example I will store user data on local storage
+						UserService.setUser({
+							authResponse: success.authResponse,
+							userID: profileInfo.id,
+							name: profileInfo.name,
+							email: profileInfo.email,
+							picture : "http://graph.facebook.com/" + success.authResponse.userID + "/picture?type=large"
+						});
+					}, function(fail){
+						// Fail get profile info
+						console.log('failed to get profile info, failure:', fail);
+						fbLoginError('local error, failed to retrieve user data');
+					});
+				}
+
+				setHelpshiftInformation(user.name, user.email);
+				$state.go('tab.dash');
+
+			} else {
+				// If (success.status === 'not_authorized') the user is logged in to Facebook,
+				// but has not authenticated your app
+				// Else the person is not logged into Facebook,
+				// so we're not sure if they are logged into this app or not.
+
+				console.log('getLoginStatus', success.status);
+
+				$ionicLoading.show({
+					template: 'Logging in...'
+				});
+
+				// Ask the permissions you need. You can learn more about
+				// FB permissions here: https://developers.facebook.com/docs/facebook-login/permissions/v2.4
+				facebookConnectPlugin.login(['email', 'public_profile'], fbLoginSuccess, fbLoginError);
+			}
+		});
+	};
+});
