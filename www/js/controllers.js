ff4fb52bcd7f6052cb6daa331a224005644332f5
swapnil agarwal <swapnil.agarwal@wingify.com>
2016-08-10 16:34:55 +0530

    added chart on dashboard

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,12 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, Blogs) {
-	$scope.blogs = Blogs.all();
+.controller('DashCtrl', function($scope) {
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+    ];
 })
 
 .controller('WeightCtrl', function($scope, $ionicPopup, $state, $firebaseAuth, $ionicHistory) {
